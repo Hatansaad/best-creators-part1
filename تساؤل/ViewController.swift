b622f88d4bd20ff6fb4c74b15a4ec3ca0bf06fe0
bashayeralsaeed <36025818+bashayeralsaeed@users.noreply.github.com>
2018-02-14 12:23:06 +0300

    .

--- تساؤل/ViewController.swift
+++ تساؤل/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor=UIColor.white//background color white
+        button1.layer.cornerRadius=button1.frame.height/2//round button
+        button2.layer.cornerRadius=button2.frame.height/2//round button
+        button3.layer.cornerRadius=button3.frame.height/2//round button
+        
     }
 
     override func didReceiveMemoryWarning() {
